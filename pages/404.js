e652331cb5048f5f523d7179b69fa6737f255de2
Saqlain Khan <iamsaqlain25@gmail.com>
2022-05-12 15:34:22 +0530

    All about Routing

---
+++ pages/404.js
@@ -0,0 +1,5 @@
+function PagenotFound() {
+  return <h1>404 Page</h1>;
+}
+
+export default PagenotFound;
---
+++ pages/profile.js
@@ -0,0 +1,5 @@
+function Profile() {
+  return <h1>About Profile</h1>;
+}
+
+export default Profile;
